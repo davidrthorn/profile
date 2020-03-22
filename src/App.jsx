9f3b7bdeff9653d167ef886c232656fd8bc77c05
davidr <davidrowthorn@gmail.com>
2020-03-22 21:08:34 +0000

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const App = ({ name }) => {
+  return (
+    <h1>Hello {name}</h1>
+  )
+}
+
+App.propTypes = {
+  name: PropTypes.string
+}
+
+export default App
